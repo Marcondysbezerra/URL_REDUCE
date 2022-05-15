83f9a0298b280716b983cb7a3b02bfd6f3f468d2
Marcondys Bezerra <marcondysbezerra@gmail.com>
2022-05-15 09:57:15 -0300

    Terceiro commit da implementação do redutor de urls

--- devpro/encurtador/admin.py
+++ devpro/encurtador/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
-from devpro.encurtador.models import UrlRedirect
+from devpro.encurtador.models import UrlRedirect, UrlLog
 
 
 @admin.register(UrlRedirect)
 class UrlRedirectAdmin(admin.ModelAdmin):
     list_display = ('destino', 'slug', 'criado_em', 'atualizado_em')
+
+
+@admin.register(UrlLog)
+class UrlLogAdmin(admin.ModelAdmin):
+    list_display = ('origem', 'user_agent', 'criado_em', 'host', 'ip', 'url_redirect')
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request):
+        return False
